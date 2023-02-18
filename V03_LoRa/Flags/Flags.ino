e66568cc4c3cb6eb6612c035646dfe89d9c04fc1
Samuel Eames <samueljeames@gmail.com>
2023-02-18 15:33:54 +1000

    Implemented flag col selecting at power up

--- V03_LoRa/Flags/Flags.ino
+++ V03_LoRa/Flags/Flags.ino
@@ -79,17 +79,19 @@ const uint32_t IR_FlagCodes[] = {
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, LED_DATA_PIN, NEO_GRB + NEO_KHZ800);                       
 
 // Use following two variables together e.g. teamcols_Hue(RED)
-enum Colours:uint8_t { BLACK, RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, MAGENTA, WHITE};
-const uint32_t teamCols[] =   {  0x000000,   // Black
+enum Colours:uint8_t { RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, MAGENTA, WHITE, BLACK, GREY };
+const uint32_t teamCols[] =   {  
                                  0xFF0000,   // Red
-                                 0xFF1C00,   // Orange
-                                 0xFF6F00,   // Yellow
-                                 0x00FF0A,   // Green
+                                 0xFF2F00,   // Orange
+                                 0xFF7F00,   // Yellow
+                                 0x00FF08,   // Green
                                  0x00AAAA,   // Cyan
                                  0x0000FF,   // Blue
-                                 0x8F00FF,   // Purple
+                                 0x9F00FF,   // Purple
                                  0xFF002F,   // Magenta
-                                 0xFFFFFF    // White
+                                 0xFFFFFF,   // White
+                                 0x000000,   // Black
+                                 0x050505    // Grey
                               };
 
 
@@ -97,10 +99,11 @@ const uint32_t teamCols[] =   {  0x000000,   // Black
 #define BTN_PIN 0
 #define DEBOUNCE 100       // (ms) Button debounce time
 #define BTN_PRESS_LONG 1000   // (ms) Min time for a 'long' button press
-uint8_t myFlagCol;
-uint8_t myFlagID;
+uint8_t myFlagCol;            // Holds colour to be used for this flag
+uint8_t myFlagID;             // Holds ID to be used for this flag
 
-#define MAX_FLAG_ID    10  // Number of unique flag IDs
+#define MAX_FLAG_ID    16  // Number of unique flag IDs per colour - note; value loosely limited by number of pixel LEDs to show ID value 
+#define MAX_FLAG_COLS  10 // Number of unique flag colours
 
 enum EEPROM_DATA:uint8_t { DATA_COL, DATA_ID }; // locations in EEPROM that data is saved to
 
@@ -125,6 +128,15 @@ void setup()
    myFlagCol = EEPROM.read(DATA_COL);
    myFlagID  = EEPROM.read(DATA_ID);
 
+   uint8_t temp_FlagCol = myFlagCol;   // Used for checking EEPROM isn't resaving same value
+   uint8_t temp_FlagID  = myFlagID;
+
+   // Check values are within range
+   if (myFlagCol >= MAX_FLAG_COLS)
+      myFlagCol = WHITE;
+   if (myFlagID >= MAX_FLAG_ID)
+      myFlagID = 0;
+
 
    // Check for setup
    if (!digitalRead(BTN_PIN))
@@ -132,24 +144,36 @@ void setup()
       flashLED(WHITE, 3, 100, 10);
 
       // Set colour
-      while (setFlagColID(myFlagCol, sizeof(teamCols)))
-         fill_solid(teamCols[myFlagCol]);
+      while (!setFlagColID(myFlagCol, MAX_FLAG_COLS))
+         fill_solid(myFlagCol);
 
       // Set ID Num
-      while(setFlagColID(myFlagID, MAX_FLAG_ID))
-         flashLED_NB(teamCols[myFlagCol], myFlagID, 100, 500);
+      while(!setFlagColID(myFlagID, MAX_FLAG_ID))
+         showLED_ID(myFlagCol, myFlagID);
+         // flashLED_NB(myFlagCol, myFlagID, 100, 500);
 
       // Save the things
-      EEPROM.write(DATA_COL, myFlagCol);
-      EEPROM.write(DATA_ID, myFlagID);
+      if (myFlagCol != temp_FlagCol)
+         EEPROM.write(DATA_COL, myFlagCol);
+      if (myFlagID != temp_FlagID)
+         EEPROM.write(DATA_ID, myFlagID);
 
       // Continue startup
+      flashLED(WHITE, 3, 100, 10);
    }
 
 
    // Flash LEDs according to flag ID
+   showLED_ID(myFlagCol, myFlagID);
+
+   uint32_t startTime = millis();
+   while (millis() - startTime < BTN_PRESS_LONG); // Pause
+
+   flashLED(myFlagCol, 3, 50, 0);
 
    // Finish with flag lit up (solid) colour
+   fill_solid(myFlagCol);
+
 }
  
 //////////////////////////////////////////////////
@@ -179,42 +203,58 @@ void flashLED(uint8_t flashCol, uint8_t flashNum, uint8_t flashTime, uint8_t pau
 }
 
 
-bool flashLED_NB(uint8_t flashCol, uint8_t flashNum, uint8_t flashTime, uint8_t pause)
+// bool flashLED_NB(uint8_t flashCol, uint8_t flashNum, uint8_t flashTime, uint8_t pause)
+// {
+//    // Flashes pixel LEDs given colour, number of flashes, and timing (ms)
+//    // NON-BLOCKING --> call function a lot!
+
+
+//    static uint8_t i;
+//    static uint32_t startTime = millis();
+
+//    if (i < flashNum*2)
+//    {
+//       if (millis() - startTime >= flashTime)
+//       {
+//          if (i % 2)
+//             fill_solid(flashCol);
+//          else
+//             fill_solid(BLACK);
+
+//          startTime = millis();
+//          i++;
+//       }
+//    }
+//    else
+//    {
+//       if (millis() - startTime >= pause)        // Reset variables
+//       {
+//          i = 0;
+//          startTime = millis();
+//          return true;
+//       }
+//    }
+
+
+//    return false;
+// }
+
+void showLED_ID(uint8_t fillcolour, uint8_t ID)
 {
-   // Flashes pixel LEDs given colour, number of flashes, and timing (ms)
-   // NON-BLOCKING --> call function a lot!
+   // Lights LEDs in binary pattern according to ID
 
-
-   static uint8_t i;
-   static uint32_t startTime = millis();
-
-   if (i < flashNum*2)
+   for (uint8_t i = 0; i < NUM_LEDS; ++i)
    {
-      if (millis() - startTime >= flashTime)
-      {
-         if (i % 2)
-            fill_solid(flashCol);
-         else
-            fill_solid(BLACK);
-
-         startTime = millis();
-         i++;
-      }
-   }
-   else
-   {
-      if (millis() - startTime >= pause)        // Reset variables
-      {
-         i = 0;
-         startTime = millis();
-         return true;
-      }
+      if ((ID >> i) & 1)
+         pixels.setPixelColor(i, teamCols[fillcolour]);
+      else
+         pixels.setPixelColor(i, teamCols[GREY]);
    }
 
-
-   return false;
+   pixels.show();
+   
+   return;
 }
-
 
 
 
@@ -241,20 +281,25 @@ bool setFlagColID(uint8_t &value, uint8_t max)
    bool btnState = digitalRead(BTN_PIN);            // Read button state
 
 
-   if (!btnState && lastBtnState != btnState)      // If button just pressed
+   if (!btnState && !lastBtnState && (millis() - pressedTime > BTN_PRESS_LONG)) // LONG button press
+   {
+      // Reset values
+      if (value == 0)                              // Take it back a step
+         value = max-1;
+      else
+         value--;      
+
+      pressedTime = millis();
+      lastBtnState = true;
+      return true;
+   }
+   else if (!btnState && lastBtnState != btnState)      // If button just pressed
    {
       value++;                                     // Increment value
       if (value >= max)                            // Ensure we're within bounds
          value = 0;
 
       pressedTime = millis();
-   }
-   else if (!btnState && !lastBtnState && (millis() - pressedTime > BTN_PRESS_LONG))
-   {
-      // Reset values
-      pressedTime = millis();
-      lastBtnState = true;
-      return true;
    }
 
 
